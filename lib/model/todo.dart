ba10a4a940ce01f454c3aff417ca9588ac0117e4
Mehta Pavan <mehtapavan55@gmail.com>
2023-01-21 00:37:19 +0530

    Initial commit

---
+++ lib/model/todo.dart
@@ -0,0 +1,16 @@
+class ToDo {
+  String? id;
+  String? todoText;
+  bool isDone;
+
+  ToDo({
+    required this.id,
+    required this.todoText,
+    this.isDone = false,
+  });
+
+  static List<ToDo> todoList() {
+    return [
+    ];
+  }
+}
